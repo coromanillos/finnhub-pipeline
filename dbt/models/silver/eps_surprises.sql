fd64ba8e8820b14a69e5fdcc5bf68631d04af107
Christopher <cromanillos99@cromDebDesktop.myfiosgateway.com>
2026-04-03 13:33:15 -0400

    silver schema layer completed

--- dbt/models/silver/eps_surprises.sql
+++ dbt/models/silver/eps_surprises.sql
@@ -1,15 +1,15 @@
 -- models/silver/eps_surprises.sql
-
+-- GOOD
 SELECT
     ticker,
     pulled_at,
-    data:actual::FLOAT          AS actual,
-    data:estimate::FLOAT        AS estimate,
-    data:period::VARCHAR        AS period,
-    data:quarter::VARCHAR       AS quarter,
-    data:surprise::FLOAT        AS surprise,
-    data:surprisePercent::FLOAT AS surprise_percent,
-    data:symbol::VARCHAR        AS symbol,
-    data:year::VARCHAR          AS year
+    entry.value:actual::FLOAT           AS actual,
+    entry.value:estimate::FLOAT         AS estimate,
+    entry.value:period::VARCHAR         AS period,
+    entry.value:quarter::INT            AS quarter,
+    entry.value:surprise::FLOAT         AS surprise,
+    entry.value:surprisePercent::FLOAT  AS surprise_percent,
+    entry.value:symbol::VARCHAR         AS symbol,
+    entry.value:year::INT               AS year
 FROM FINNHUB.FINNHUB_BRONZE.eps_surprises_raw,
-LATERAL FLATTEN(input => data:data)  -- Unnest the array
+    LATERAL FLATTEN(input => data:data) AS entry
